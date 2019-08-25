ae05b08ee2dabe732cabcdc3c1c91b57e1c72468
David Rupe <DavidFRupe@hotmail.com>
2019-08-25 15:07:47 -0700

    Adding ShowMesh.com ASP.Net MVC project.

---
+++ ShowMesh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShowMesh.MvcApplication" Language="C#" %>
---
+++ ShowMesh/Models/ShowTitle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShowMesh.Models
+{
+    public class ShowTitle
+    {
+    }
+}
